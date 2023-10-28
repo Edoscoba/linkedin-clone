3aeb1596e5a30dfb080b5f03c6f1e71a2034fa7d
Obi <obisimon88@gmail.com>
2023-10-27 22:46:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+//  import { Route, Routes } from 'react-router-dom';
+
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
 
 function App() {
+  alert(`Please bear with me,\n i was not able to complete it cause of some errors i have being getting \n Promising to complete it in no time`)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     {/* <Routes>
+       <Route path="/Login" element={<Login/>}> </Route>
+  
+    </Routes> */}
+  
+     <Login/>
+    <Header/>
+    <Home/>
+   </div>
+
   );
 }
 
